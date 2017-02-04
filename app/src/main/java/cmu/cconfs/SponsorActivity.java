27ed5bb2ec089d6966a5927ed6d92419033c2dce
jialingliu <jialing.whu@gmail.com>
2017-02-04 11:24:02 -0800

    partialy integrate sponsor with drawer

--- app/src/main/java/cmu/cconfs/SponsorActivity.java
+++ app/src/main/java/cmu/cconfs/SponsorActivity.java
@@ -1,12 +1,25 @@
 package cmu.cconfs;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.github.florent37.materialviewpager.MaterialViewPager;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 
@@ -15,6 +28,8 @@ import java.util.List;
 
 import cmu.cconfs.adapter.SponsorListAdapter;
 import cmu.cconfs.model.parseModel.Sponsor;
+import cmu.cconfs.parseUtils.helper.LoadingUtils;
+import cmu.cconfs.utils.PreferencesManager;
 
 
 public class SponsorActivity extends AppCompatActivity {
@@ -22,6 +37,15 @@ public class SponsorActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     SponsorListAdapter adapter;
 
+    private final static String TAG = RoomScheduleActivity.class.getSimpleName();
+    private MaterialViewPager mViewPager;
+    // navigate toobar
+    public static PreferencesManager mPreferencesManager;
+    private DrawerLayout mDrawerLayout;
+    private Toolbar mToolbar;
+    private NavigationView mNavigationView;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private final static int REQUEST_SIGN_IN = HomeActivity.REQUEST_SIGN_IN;
     List<Sponsor> sponsors = new ArrayList<>();
 
     @Override
@@ -29,18 +53,20 @@ public class SponsorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_sponsor);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        mPreferencesManager = new PreferencesManager(this);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        toolbar.inflateMenu(R.menu.menu_paper);
-        setSupportActionBar(toolbar);
-        ActionBar actionBar = getSupportActionBar();
+//        setSupportActionBar(toolbar);
+        setTitle("Sponsor");
 
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setDisplayShowHomeEnabled(true);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setDisplayUseLogoEnabled(false);
-        actionBar.setHomeButtonEnabled(true);
-        actionBar.setTitle("Sponsor");
+//        ActionBar actionBar = getSupportActionBar();
+//
+////        actionBar.setDisplayHomeAsUpEnabled(true);
+//        actionBar.setDisplayShowHomeEnabled(true);
+//        actionBar.setDisplayShowTitleEnabled(true);
+//        actionBar.setDisplayUseLogoEnabled(false);
+//        actionBar.setHomeButtonEnabled(true);
+//        actionBar.setTitle("Sponsor");
 
         recyclerView = (RecyclerView) findViewById(R.id.recycler);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
@@ -51,9 +77,119 @@ public class SponsorActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
 
         populate();
+        // set up the nav drawer
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+        getSupportActionBar().setTitle("Sponsor");
 
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mNavigationView = (NavigationView) findViewById(R.id.nvView);
+        mDrawerToggle = setupDrawerToggle();
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+
+        setupNavigationView(mNavigationView);
+    }
+    private void setupNavigationView(NavigationView navView) {
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                selectDrawerItem(menuItem);
+                return true;
+            }
+        });
     }
 
+    private void selectDrawerItem(MenuItem menuItem) {
+        Intent i = new Intent();
+        switch (menuItem.getItemId()) {
+            case R.id.nav_dash_board:
+                Toast.makeText(getApplicationContext(), "dash board clicked", Toast.LENGTH_SHORT).show();
+                i.setClass(this, HomeActivity.class);
+                startActivity(i);
+                break;
+            case R.id.nav_my_schedule:
+                Toast.makeText(getApplicationContext(), "my schedule clicked", Toast.LENGTH_SHORT).show();
+                i.setClass(getApplicationContext(), ScheduleActivity.class);
+                startActivity(i);
+                break;
+            case R.id.nav_my_profile:
+                Toast.makeText(getApplicationContext(), "my profile clicked", Toast.LENGTH_SHORT).show();
+                i = getLoginStatusIntent(ProfileActivity.class, LoginActivity.class);
+                startActivityForResult(i, REQUEST_SIGN_IN);
+                break;
+            case R.id.nav_my_to_do_list:
+                Toast.makeText(getApplicationContext(), "my todo list clicked", Toast.LENGTH_SHORT).show();
+                i.setClass(this, TodoListActivity.class);
+                startActivity(i);
+                break;
+            case R.id.nav_sync_data:
+                syncScheduleData(this);
+                break;
+            case R.id.nav_log_in:
+                i = getLoginStatusIntent(UserActivity.class, LoginActivity.class);
+                startActivityForResult(i, REQUEST_SIGN_IN);
+                break;
+        }
+
+        menuItem.setCheckable(true);
+        setTitle(menuItem.getTitle());
+        mDrawerLayout.closeDrawers();
+    }
+
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        return new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.drawer_open,  R.string.drawer_close);
+    }
+    private Intent getLoginStatusIntent(Class loginTarget, Class notLoginTarget) {
+        boolean loggedIn = mPreferencesManager.getBooleanPreference("LoggedIn",false);
+        Toast .makeText(this, loggedIn + "", Toast.LENGTH_SHORT).show();
+        Intent i = new Intent();
+        if(!loggedIn) {
+            i.setClass(getApplicationContext(), notLoginTarget);
+        } else {
+            i.setClass(getApplicationContext(), loginTarget);
+        }
+        return i;
+    }
+
+    private void syncScheduleData(final Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.MyDialogTheme);
+        builder.setTitle("Sync Data");
+        builder.setMessage("You sure want to reload the backend data?");
+        builder.setPositiveButton(getString(android.R.string.ok), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                final ProgressDialog pd = new ProgressDialog(context);
+                String st = "Syncing data...";
+                pd.setMessage(st);
+                pd.setCanceledOnTouchOutside(false);
+                pd.show();
+                new AsyncTask<Void, Void, Void>() {
+                    @Override
+                    protected Void doInBackground(Void... voids) {
+                        try {
+                            LoadingUtils.loadFromParse();
+                            LoadingUtils.populateDataProvider();
+                            LoadingUtils.populateRoomProvider();
+                        } catch (ParseException e) {
+                            Log.e(TAG, e.getMessage());
+                        }
+                        return null;
+                    }
+
+                    @Override
+                    protected void onPostExecute(Void aVoid) {
+                        pd.dismiss();
+                    }
+                }.execute();
+            }
+        });
+        builder.setNegativeButton(getString(android.R.string.cancel), null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
     private void populate() {
         ParseQuery<Sponsor> query = Sponsor.getQuery();
         query.fromLocalDatastore();
